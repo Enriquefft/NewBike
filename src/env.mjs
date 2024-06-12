a0ebc99d2fb953416cb90c630336f5b20d5ed97b
Enriquefft <enriquefft2001@gmail.com>
2024-06-12 12:20:16 -0500

    feat: added database connection

--- src/env.mjs
+++ src/env.mjs
@@ -6,9 +6,6 @@ export const env = createEnv({
   server: {
     DRIZZLE_DATABASE_URL: z.string().url(),
 
-    AUTH_GOOGLE_ID: z.string(),
-    AUTH_GOOGLE_SECRET: z.string(),
-    AUTH_SECRET: z.string(),
   },
   client: {
     NEXT_PUBLIC_PROJECT_NAME: z.string(),
@@ -16,10 +13,6 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_PROJECT_NAME: process.env.NEXT_PUBLIC_PROJECT_NAME,
     DRIZZLE_DATABASE_URL: process.env.DRIZZLE_DATABASE_URL,
-
-    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
-    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
-    AUTH_SECRET: process.env.AUTH_SECRET,
   },
   /*
    * For Next.js >= 13.4.4, you only need to destructure client variables:
